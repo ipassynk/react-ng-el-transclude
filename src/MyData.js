d87fb75aebe623e82a8ec9207df07f8ab03ba892
Julia.Passynkova <julia.passynkova@nurun.com>
2016-08-14 17:36:21 -0400

    Use jsx for MyData`

--- src/MyData.js
+++ src/MyData.js
@@ -3,8 +3,11 @@ import React, {Component} from "react";
 class MyData extends Component {
     render() {
         if (this.props.withContainer) {
-            let el = React.createElement(this.props.withContainer, null, <MyData/>);
-            return (el);
+            return (
+                <this.props.withContainer>
+                    <MyData/>
+                </this.props.withContainer>
+            );
         }
         else {
             return (<div>My Data is here</div>);
