b0179f4a5c7fc32841a6c7c60eac54d6c05bf1b2
Julia.Passynkova <julia.passynkova@nurun.com>
2016-08-14 22:13:14 -0400

    Use props.children

--- src/MyData.js
+++ src/MyData.js
@@ -1,13 +1,9 @@
 import React, {Component} from "react";
 
-let Container = (props) => <props.withContainer><MyData/></props.withContainer>
-
 class MyData extends Component {
     render() {
         return (
-            <div>
-                {this.props.withContainer ? <Container {...this.props}/> : <div>My Data is here</div>}
-            </div>
+            <this.props.withContainer>{this.props.children}</this.props.withContainer>
         );
     }
 }
